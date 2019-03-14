6dabc8562a47eb64c2be23352f21805ba1a18b67
Bany <banyhong@gliacloud.com>
2019-03-14 12:52:56 +0800

    use thread to play sound

--- issue.py
+++ issue.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
-from flask import Flask, request, abort
+from threading import Thread
+from flask import Flask, request, abort, jsonify
 
 app = Flask(__name__)
 
@@ -13,21 +14,27 @@ for fn in os.listdir('closed'):
     close_sound[os.path.splitext(fn)[0]] = os.path.join('closed', fn)
 
 
+def play_sound(sound_file):
+    subprocess.run(['omxplayer', '--no-keys', '--no-osd', sound_file])
+
+
 @app.route('/github_issues', methods=['POST'])
 def webhook():
     if request.method == 'POST':
         data = request.json
-        print(data)
         if data.get('action') == 'opened':
             user = data['issue']['user']['login']
             sound_file = open_sound.get(user, open_sound['default'])
-            subprocess.run(['omxplayer', sound_file])
+            Thread(target=play_sound, args=(sound_file,)).start()
             print('open', user, sound_file)
         elif data['action'] == 'closed':
             user = data['issue']['assignee']
+            if user is not None:
+                user = user['login']
             sound_file = close_sound.get(user, close_sound['default'])
-            subprocess.run(['omxplayer', sound_file])
+            Thread(target=play_sound, args=(sound_file,)).start()
             print('close', user, sound_file)
+        return jsonify({'status':'success'}), 200
     else:
         abort(400)
 
